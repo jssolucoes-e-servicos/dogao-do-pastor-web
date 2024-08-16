fb5812d2a4557dea68e14356b7ca6f2d1597263e
JS Soluções e Serviços <jssolucoeseservicos@gmail.com>
2024-08-16 09:26:26 -0300

    change redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
 import api from "@/services/api";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("acl0")?.value;
   const userCookie = request.cookies.get("acl1")?.value;
-  if (request.nextUrl.pathname.startsWith("/login")) {
+  if (request.nextUrl.pathname.startsWith("/acesso")) {
     if (token && userCookie) {
       api.defaults.headers.Authorization = `Bearer ${token}`;
-      return NextResponse.redirect(new URL("/dashboard", request.url));
+      return NextResponse.redirect(new URL("/app", request.url));
     }
     return null;
   }
-  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+  if (request.nextUrl.pathname.startsWith("/app")) {
     if (!token || !userCookie) {
-      return NextResponse.redirect(new URL("/login", request.url));
+      return NextResponse.redirect(new URL("/acesso", request.url));
     }
     api.defaults.headers.Authorization = `Bearer ${token}`;
   }
@@ -21,33 +21,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/login", "/dashboard/:path*"],
+  matcher: ["/acesso", "/app/:path*"],
 };
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function middleware(request: NextRequest) {
-//   const token = request.cookies.get("acl0")?.value;
-//   const userCookie = request.cookies.get("acl1")?.value;
-//   if (request.nextUrl.pathname.startsWith("/login")) {
-//     if (token && userCookie) {
-//       return NextResponse.redirect(new URL("/app", request.url));
-//     }
-//     return null;
-//   }
-//   if (request.nextUrl.pathname.startsWith("/app")) {
-//     if (!token || !userCookie) {
-//       return NextResponse.redirect(new URL("/login", request.url));
-//     }
-//     if(request.nextUrl.pathname.startsWith("/app/dashboard")) {
-//       const user = JSON.parse(userCookie);
-//       if (user?.rule !== "admin") {
-//         return NextResponse.redirect(new URL("/app", request.url));
-//       }
-//     }
-//   }
-//   return null;
-// }
-
-// export const config = {
-//   matcher: ["/login", "/app/:path*"],
-// };
